d2cc6522f06e90d3d4b43451628d52cc4b770dae
Tomislav <tdumancic10@gmail.com>
2020-01-13 12:18:09 +0100

    1 commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,45 @@ import "babel-polyfill";
 
 // \/ All of your javascript should go here \/
 
+let submitButton = document.querySelector('.btn')
+let form = document.querySelector('#submit-form')
+let name = document.querySelector('#name')
+let email = document.querySelector('#email')
+let password = document.querySelector('#password')
+let message = document.querySelector('#message')
+let checkbox = document.querySelector('#checkbox')
+
+submitButton.addEventListener('click',myFunction)
+
+ async function  myFunction(e){
+    e.preventDefault()
+    alert('Thank you for submiting your details')
+    
+    
+
+    let url = 'https://jsonplaceholder.typicode.com/posts'
+
+    let data = await fetch(url,{
+        method: 'POST',
+        body: JSON.stringify({
+            name:name.value,
+            email:email.value,
+            password:password.value,
+            message:message.value,
+            checkbox:checkbox.checked,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+    })
+    let request = await data.json()
+
+    let string = JSON.stringify(request)
+    
+    return console.log(string)
+
+    
+
+}
+
+ 
